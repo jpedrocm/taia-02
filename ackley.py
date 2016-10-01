2865309e317e65f0714ab1c721da19cfa9796036
vinimoraesrc <vmrc@cin.ufpe.br>
2016-10-01 16:49:12 -0300

    first working version with success rule

--- ackley.py
+++ ackley.py
@@ -1,13 +1,23 @@
 import math
 import random
+import sys
 
-POPULATION_SIZE = 10
-LAMBDA = 20
+POPULATION_SIZE = 50
+# Number of children
+LAMBDA = 200
+# Whether or not to use fixed 2 parents for each individual
 FIXED_PARENTS = True
+# Whether or not to use mu + lambda survivor selection
+MU_PLUS_LAMBDA = False
 
-NUM_DIMENSIONS = 3
+# Constant step factor
+C = 0.9
+
+NUM_DIMENSIONS = 5
 X_MIN = -15
 X_MAX = 15
+
+NUM_ITERATIONS = 1000
 
 def generate_population():
     population = list()
@@ -35,7 +45,7 @@ def recombination(population):
     for i in range(NUM_DIMENSIONS):
         xi = parents[0][i]
         xj = parents[1][i]
-        child.append((xi + xj) / 2)
+        child.append(float(xi + xj) / 2)
         if not FIXED_PARENTS:
             parents = select_parents(population)
     return child
@@ -47,4 +57,59 @@ def generate_offspring(population):
         offspring.append(child)
     return offspring
 
-population = generate_population()
+def mutation(child, Z):
+    mutated = list()
+    for i in range(NUM_DIMENSIONS):
+        mutated.append(child[i] + Z[i])
+    return child if ackley(child) <= ackley(mutated) else mutated
+
+def perturbation(offspring, sigma):
+    Z = [random.gauss(0, sigma) for i in range(NUM_DIMENSIONS)]
+    new_offspring = list()
+    s = 0
+    t = 0
+    for child in offspring:
+        new_child = mutation(child, Z)
+        new_offspring.append(new_child)
+        if new_child != child:
+            s += 1
+        t += 1
+    return (new_offspring, float(s) / t)
+    
+def adjust_sigma(sigma, ps):
+    if ps > 0.2:
+        return float(sigma) / C
+    elif ps < 0.2:
+        return sigma * C
+    return sigma
+
+def select_survivors(population, offspring):
+    new_population = population + offspring if MU_PLUS_LAMBDA else offspring  
+    new_population.sort(key = lambda x : ackley(x))
+    new_population = new_population[:POPULATION_SIZE]
+    return new_population
+
+def check_for_solution(population):
+    for x in population:
+        # By using sys.float_info.epsilon, which equals to ~10e-16 in my machine,
+        # the convergence rate is greatly diminished. Using 10e-6 instead.
+        if abs(ackley(x)) < 10e-6:
+            return True
+    return False
+
+def evolve():
+    population = generate_population()
+    sigma = 1.0
+    for i in range(NUM_ITERATIONS):
+        offspring = generate_offspring(population)
+        (offspring, ps) = perturbation(offspring, sigma)
+        sigma = adjust_sigma(sigma, ps)
+        if check_for_solution(offspring):
+            print "Solution found after " + str(i) + " iterations"
+            return
+        population = select_survivors(population, offspring)
+        # DEBUG
+        #print min(map(lambda x : ackley(x), population))
+    print "No solution found after " + str(NUM_ITERATIONS) + " iterations"
+
+evolve()
