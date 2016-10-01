93956abf0fe85c87b9cd536590533c9b9b67012a
vinimoraesrc <vmrc@cin.ufpe.br>
2016-10-01 15:24:06 -0300

    Ãadds offspring generation, parent selection and recombination

--- ackley.py
+++ ackley.py
@@ -1,8 +1,11 @@
 import math
 import random
 
-POPULATION_SIZE = 100
-NUM_DIMENSIONS = 30
+POPULATION_SIZE = 10
+LAMBDA = 20
+FIXED_PARENTS = True
+
+NUM_DIMENSIONS = 3
 X_MIN = -15
 X_MAX = 15
 
@@ -22,3 +25,26 @@ def ackley(genome):
     a = -0.2 * math.sqrt((1.0 / n) * sum1)
     b = (1.0 / n) * sum2
     return -20 * math.exp(a) - math.exp(b) + 20 + math.e
+
+def select_parents(population):
+    return random.sample(population, 2)
+
+def recombination(population):
+    parents = select_parents(population)
+    child = list()
+    for i in range(NUM_DIMENSIONS):
+        xi = parents[0][i]
+        xj = parents[1][i]
+        child.append((xi + xj) / 2)
+        if not FIXED_PARENTS:
+            parents = select_parents(population)
+    return child
+
+def generate_offspring(population):
+    offspring = list()
+    for i in range(LAMBDA):
+        child = recombination(population)
+        offspring.append(child)
+    return offspring
+
+population = generate_population()
